f52331c8146a3c2576821da442a42a00ae80d7e9
Adam K Gray <akg1852@server.fake>
2014-12-27 10:23:59 +1300

    make tally fully case insensitive

--- commands/tally.js
+++ commands/tally.js
@@ -11,50 +11,35 @@ module.exports = {
 		dbConnect(function(db) {
 			db.run("CREATE TABLE IF NOT EXISTS tally" +
 				"(category TEXT, member TEXT, count INT, PRIMARY KEY (category, member))", function(error) {
-				if (error) {
-					console.error("Error creating tally table in database: " + JSON.stringify(error));
-				}
-				else {
-					db.all("SELECT * FROM tally WHERE category LIKE ? AND member LIKE ?",
-						category, member || "%", function(error, rows) {
-						if (error) {
-							console.error("Error reading tally: " + JSON.stringify(error));
+				if (error) return console.error("Error creating tally table in database: " + JSON.stringify(error));
+				
+				db.all("SELECT * FROM tally WHERE category LIKE ? AND member LIKE ?", category, member || "%", function(error, rows) {
+					var postTally = function(rows) {
+						var result = "";
+						for (var i = 0; i < rows.length; i++) {
+							result += "\n\t" + rows[i].member + ": " + rows[i].count;
 						}
-						else {
-							var postTally = function(rows) {
-								var result = "";
-								for (var i = 0; i < rows.length; i++) {
-									result += "\n\t" + rows[i].member + ": " + rows[i].count;
-								}
-								post("Tally for " + category + ":" + result, context, callback);
-							};
-							if (member) {
-								var count = rows.length ? rows[0].count : 0;
-								if (command) {
-									eval("count" + command);
-									if (count) {
-										db.run("INSERT OR REPLACE INTO tally VALUES (?, ?, ?)",
-											category, member, count, function(error) {
-											if (error) {
-												console.error("Error updating tally: " + JSON.stringify(error));
-											}
-											else postTally([{category: category, member: member, count: count}]);
-										});
-									}
-									else {
-										db.run("DELETE FROM tally WHERE category = ? AND member = ?",
-											category, member, function(error) {
-											if (error) console.error("Error deleting tally: " + JSON.stringify(error));
-											else postTally([{category: category, member: member, count: count}]);
-										});
-									}
+						post("Tally for " + category + ":" + result, context, callback);
+					};
+					
+					if (error) return console.error("Error reading tally: " + JSON.stringify(error));
+					if (!command) return postTally(rows);
+					
+					var count = rows.length ? rows[0].count : 0;
+					eval("count" + command);
+					db.run("DELETE FROM tally WHERE category LIKE ? AND member LIKE ?", category, member, function(error) {
+						if (error) console.error("Error deleting tally: " + JSON.stringify(error));
+						else if (count) {
+							db.run("INSERT INTO tally VALUES (?, ?, ?)", category, member, count, function(error) {
+								if (error) {
+									console.error("Error updating tally: " + JSON.stringify(error));
 								}
 								else postTally([{category: category, member: member, count: count}]);
-							}
-							else postTally(rows);
+							});
 						}
+						else postTally([{category: category, member: member, count: count}]);
 					});
-				}
+				});
 			});
 		});
 	}
