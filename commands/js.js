12ed34d79a08b61f92fb12ea29c8859b6c41b046
Adam K Gray <akg1852@server.fake>
2015-02-16 19:00:54 +1300

    allow js command to run code without defining a function first and add ability to print a function's definition

--- commands/js.js
+++ commands/js.js
@@ -9,9 +9,10 @@ var shortcircuit = false;
 
 module.exports = {
 	description: "js {name}({params}) [\\n {code}]: create/run custom js code\n\t\t\t\t\t(see: 'js help')",
-	pattern: /^js\s+(?:(help|list|shortcircuit|delete\s+(\S+))|(?:(\S+)\s*\(([^\n\r]*)\)\s*(?:[\n\r]+([\s\S]+))?))$/i,
+	pattern: /^js(?:\s+(?:(help)|(list)|(shortcircuit)|(delete)))?(?:[^\S\n\t]+([A-Z_$][0-9A-Z_$]*))?(?:[^\S\n\r]*\(([^\n\r]*)\))?(?:[\n\r]+([\s\S]+))?$/i,
 	priority: -10,
 	reply: function(match, context, callback) {
+		var name = match[5], params = match[6], code = match[7];
 		dbConnect(function(db) {
 			db.run("CREATE TABLE IF NOT EXISTS js" +
 				"(name TEXT, params TEXT, code TEXT, deleted INTEGER)", function(error) {
@@ -28,57 +29,55 @@ module.exports = {
 				if (error) {
 					console.error("Error creating js table in database: " + JSON.stringify(error));
 				}
-				else if (match[2]) { // delete function
-					var name = match[2];
+				else if (match[1]) { // help
+					post(["User-defined javascript functions!\n",
+						"Run some code:",
+						"\tjs \\n {code}",
+						"Define a function:",
+						"\tjs {name}({param, param, ...}) \\n {code}",
+						"Call a function:",
+						"\tjs {name}({arg, arg, ...})",
+						"Delete a function:",
+						"\tjs delete {name}",
+						"List all functions:",
+						"\tjs list",
+						"Print a function's code:",
+						"\tjs {name}",
+						"Stop a spammy function in it's tracks:",
+						"\tjs shortcircuit",
+						"Inside the {code} block of a function definition, access is provided to the following:",
+						"\tfunction post(\"{message text}\") {\n\t\t/* post a message */\n\t}",
+						"\tfunction command(\"{command text}\") {\n\t\t/* call a " + config.botName + " command */\n\t}",
+						"\tvar context = {\n\t\t/* the flowdock context of the function call */\n\t};",
+					].join("\n"), context, callback);
+				}
+				if (match[2]) { // list functions
+					db.all("SELECT * FROM js WHERE deleted = 0", function(error, rows) {
+						if (error) {
+							console.error("Error retrieving js functions from db: " + JSON.stringify(error));
+						}
+						else if (rows.length) {
+							var result = ["List of all user-defined js functions:" + when.noTrigger];
+							rows.forEach(function(r) {
+								result.push(r.name + "(" + r.params + ")");
+							});
+							post(result.join("\n\t"), context, callback);
+						}
+						else post("No js functions to display.", context, callback);
+					});
+				}
+				else if (match[3]) { // shortcircuit function
+					shortcircuit = true;
+				}
+				else if (match[4] && name) { // delete function
 					deleteFunction(name, function() {
 						if (this.changes) post("Deleted js function '" + name + "'.", context, callback);
 						else post("No js function '" + name + "', nothing deleted.", context, callback);
 					});
 				}
-				else if (match[1]) {
-					if (match[1] == "list") { // list functions
-						db.all("SELECT * FROM js WHERE deleted = 0", function(error, rows) {
-							if (error) {
-								console.error("Error retrieving js functions from db: " + JSON.stringify(error));
-							}
-							else if (rows.length) {
-								var result = ["List of all user-defined js functions:" + when.noTrigger];
-								rows.forEach(function(r) {
-									result.push(r.name + "(" + r.params + ")");
-								});
-								post(result.join("\n\t"), context, callback);
-							}
-							else post("No js functions to display.", context, callback);
-						});
-					}
-					else if (match[1] == "shortcircuit") {
-						shortcircuit = true;
-					}
-					else { // help
-						post(["User-defined javascript functions!\n",
-							"Define a function:",
-							"\tjs {name}({param, param, ...}) \\n {code}",
-							"Call a function:",
-							"\tjs {name}({arg, arg, ...})",
-							"Delete a function:",
-							"\tjs delete {name}",
-							"List all functions:",
-							"\tjs list",
-							"Stop a spammy function in it's tracks:",
-							"\tjs shortcircuit",
-							"Inside the {code} block of a function definition, access is provided to the following:",
-							"\tfunction post(\"{message text}\") {\n\t\t/* post a message */\n\t}",
-							"\tfunction command(\"{command text}\") {\n\t\t/* call a " + config.botName + " command */\n\t}",
-							"\tvar context = {\n\t\t/* the flowdock context of the function call */\n\t};",
-						].join("\n"), context, callback);
-					}
-				}
-				else if (match[5]) { // define function
-					var name = match[3];
-					var params = match[4];
-					var code = match[5];
+				else if (name && code) { // define function
 					deleteFunction(name, function() {
-						db.run("INSERT INTO js VALUES (?, ?, ?, 0)", name, params, code, function(error) {
+						db.run("INSERT INTO js VALUES (?, ?, ?, 0)", name, params || "", code, function(error) {
 							if (error) {
 								console.error("Error updating js table: " + JSON.stringify(error));
 							}
@@ -86,41 +85,17 @@ module.exports = {
 						});
 					});
 				}
-				else { // call function
-					var name = match[3];
-					var args = match[4];
+				else if (code) { // call code (without defining function)
+					callFunction("", "", code, context, callback);
+				}
+				else if (name) { // call (or print) function
 					db.get("SELECT * FROM js WHERE deleted = 0 AND name = ?", name, function(error, row) {
 						if (error) {
 							console.error("Error reading js table: " + JSON.stringify(error));
 						}
 						else if (row) {
-							var commandFlag = "<EXECUTE_BOT_COMMAND>";
-							sandbox.run("var post = print, context = " + JSON.stringify(context) +
-								", command = function(s) { print(\"" + commandFlag + "\" + s); }; (function(" +
-								row.params + "){" + row.code + "})(" + args + ")",
-								function(output) {
-									var l = output.console.length, i = 0;
-									if (l) {
-										var jsOut = function() {
-											if (i < l) {
-												var value = output.console[i++];
-												if (shortcircuit) {
-													shortcircuit = false;
-													post("Shortcircuited js function '" + name + "'.", context);
-												}
-												else {
-													if (String(value).indexOf(commandFlag) == 0) {
-														commands.execute(value.slice(commandFlag.length).trim(), context, jsOut);
-													}
-													else post(value, context, jsOut);
-												}
-											}
-											else if (callback) callback();
-										};
-										jsOut();
-									}
-									else if (callback) callback();
-								});
+							if (typeof params == "string") callFunction(row.params, params, row.code, context, callback); // call
+							else post("Definition for js function " + name + "(" + row.params + ")\n" + row.code, context, callback); // print
 						}
 						else post("There is no js function '" + name + "'.", context, callback);
 					});
@@ -129,3 +104,33 @@ module.exports = {
 		});
 	}
 };
+
+function callFunction(params, args, code, context, callback) {
+	var commandFlag = "<EXECUTE_BOT_COMMAND>";
+	sandbox.run("var post = print, context = " + JSON.stringify(context) +
+		", command = function(s) { print(\"" + commandFlag + "\" + s); }; (function(" +
+		params + "){" + code + "})(" + args + ")",
+		function(output) {
+			var l = output.console.length, i = 0;
+			if (l) {
+				var jsOut = function() {
+					if (i < l) {
+						var value = output.console[i++];
+						if (shortcircuit) {
+							shortcircuit = false;
+							post("Shortcircuited js function '" + name + "'.", context);
+						}
+						else {
+							if (String(value).indexOf(commandFlag) == 0) {
+								commands.execute(value.slice(commandFlag.length).trim(), context, jsOut);
+							}
+							else post(value, context, jsOut);
+						}
+					}
+					else if (callback) callback();
+				};
+				jsOut();
+			}
+			else if (callback) callback();
+		});
+}
