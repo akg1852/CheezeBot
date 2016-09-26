d7c3b30238595797a6cefb19a413692193380a33
Adam Gray <adam.gray@xero.com>
2016-09-26 19:34:06 +1300

    improve github prs command (allow branch & add statuses)

--- commands/pullrequests.js
+++ commands/pullrequests.js
@@ -3,14 +3,15 @@ var post = require("../flowdock.js").post;
 var request = require('request');
 
 module.exports = {
-	synopsis: "pullrequests {user}/{repo}",
+	synopsis: "pullrequests {user}/{repo} [{branch}]",
 	description: "list open pull requests",
-	pattern: /^pullrequests\s+(.+\/.+)/i,
+	pattern: /^(?:pullrequests?|prs?)\s+(\S+\/\S+)(?:\s+(\S+))?/i,
 	reply: function(match, context, callback) {
 		var repo = match[1].trim();
+		var branch = match[2] ? match[2].trim() : undefined;
 		var domain = config.githubDomain ? config.githubDomain + "/api/v3" : "https://api.github.com";
 		var options = {
-			url: encodeURI(domain + "/repos/" + repo + "/pulls?access_token=" + config.githubToken),
+			url: encodeURI(domain + "/repos/" + repo + "/pulls?access_token=" + config.githubToken + (branch ? "&base=" + branch : "")),
 			method: "GET",
 			rejectUnauthorized: false,
 			headers: { "User-Agent": config.BotName }
@@ -19,12 +20,24 @@ module.exports = {
 			if (!error && response.statusCode == 200) {
 				var prs = JSON.parse(body);
 				if (prs.length) {
-					var result = "Pull requests for " + repo + ":";
-					for (var i = 0; i < prs.length; i++) {
-						var pr = prs[i];
-						result += "\n* [" + pr.title + "](" + pr.html_url + ")";
+					var result = "Pull requests for " + repo + (branch ? " (" + branch + ")" : "") + ":";
+					var addPRs = function(index) {
+						index = index || 0;
+						if (index >= prs.length) return post(result, context, callback);
+						
+						var pr = prs[index];
+						options.url = pr._links.statuses.href.replace("/statuses/", "/status/");
+						request(options, function(error, response, body) {
+							result += "\n* [" + pr.title + "](" + pr.html_url + ")";
+							if (!error) {
+								var status = JSON.parse(body);
+								if (status.state == "failure") result += " :fire:";
+								else if (status.state == "success") result += " :white_check_mark: ";
+							}
+							addPRs(index + 1);
+						});
 					}
-					post(result, context, callback);
+					addPRs();
 				}
 				else post("no open pull requests for " + repo, context, callback);
 			}
